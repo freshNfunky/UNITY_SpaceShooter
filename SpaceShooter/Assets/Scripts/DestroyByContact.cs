4d26690983777f328534715b8ec29c467298b636
Felix S <felix@ftc-creative.de>
2016-06-14 01:50:11 +0200

    New Repository

---
+++ SpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start()
+	{
+		GameObject  gameControllerObject = GameObject.FindWithTag("GameController") ;
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GameController>();
+		} else {
+			Debug.Log ("Cannot find 'Game Controller' script");
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//Debug.Log (other.name);
+		if (other.tag == "Boundary")
+		{
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate(playerExplosion, transform.position, transform.rotation);
+			gameController.GameOver ();
+		}
+		gameController.AddScore(scoreValue);
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
